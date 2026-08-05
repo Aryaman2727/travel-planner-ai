40bb16a81d8fbf47e61c31018eec84d557619744
aryaman chaudhary <93678740+Aryaman2727@users.noreply.github.com>
2026-08-05 13:37:25 +0530

    Update next.config.js

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   env: {
-    NEXT_PUBLIC_API_URL:'https://travel-planner-ai-production-85ab.up.railway.app/api'
+    NEXT_PUBLIC_API_URL:'https://travel-planner-ai-vorm.onrender.com'
 },
 };
 
